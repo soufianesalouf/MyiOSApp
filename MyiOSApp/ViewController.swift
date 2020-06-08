7dd774902a65a862ada412aa092f530fedc2026e
Soufiane Salouf <soufiane.salouf@ibm.com>
2020-06-08 16:28:26 +0100

    Initiate project

---
+++ MyiOSApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyiOSApp
+//
+//  Created by Soufiane Salouf on 6/8/20.
+//  Copyright © 2020 Soufiane Salouf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
